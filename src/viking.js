f1a9d7a85a9c85070c239e7360e0a993c84217a7
MP <princi8@gmail.comm>
2021-01-21 07:10:59 +0100

    v12
    v1

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,38 @@
 // Soldier
-class Soldier {}
+class Soldier  {
+    constructor(health, strength) { 
+        this.health = health;
+        this.strength = strength;    
+    }   
+    attack () {
+       return this.strength;
+    }
+
+    receiveDamage (damage){
+        this.health = this.health-damage;
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+       constructor(name,health, strength){
+           super(health, strength);
+           this.name=name;
+       } 
+
+       attack () {
+        return this.strength;
+       }
+
+}
+
+function receiveDamage (damage){
+    console.log("${name} has received ${damage) points of damage");
+}
+
+
+
 
 // Saxon
 class Saxon {}
